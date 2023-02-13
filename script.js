b65f4ec9c727725eae06a3315eb59df3ccd9f23a
rakabgs27 <rakabgs2000@gmail.com>
2023-02-13 10:10:07 +0700

    Client Side Rendering

--- script.js
+++ script.js
@@ -5,21 +5,52 @@ const root = document.getElementById("root");
 //Fungsi On Click untuk Link 
 linkHome.onclick = function(event) {
     event.preventDefault();
-    root.textContent = "Welcome to Home Page"
+    const homeScreen = HomeScreen();
+    root.innerHTML = "";
+    root.append(homeScreen);
     history.pushState(null,"",event.target.href)
 };
 
 //Fungsi On Click untuk Link 
 linkAbout.onclick = function(event) {
     event.preventDefault();
-    root.textContent = "Welcome to About Page"
+    const aboutScreen = AboutScreen();
+    root.innerHTML = "";
+    root.append(aboutScreen);
     history.pushState(null,"",event.target.href)
 };
 
 //Fungsi Percabangan untuk melakukan pengisian manual pada URL
 if (location.hash === "#about") {
-    root.textContent = "Welcome to About Page";
+    const aboutScreen = AboutScreen();
+    root.innerHTML = "";
+    root.append(aboutScreen);
 } else if (location.hash === "#home") {
-    root.textContent = "Welcome to Home Page";
+    const homeScreen = HomeScreen();
+    root.innerHTML = "";
+    root.append(homeScreen);
 }
 
+//Fungsi untuk Home Screen
+function HomeScreen() {
+    const textPreview = document.createElement("p");
+   
+    const input = document.createElement("input");
+    input.oninput = function (event) {
+        textPreview.textContent = event.target.value;
+    };
+    input.placeholder = "Enter your name";
+   
+    const div = document.createElement("div");
+    div.append(input);
+    div.append(textPreview);
+
+    return div;
+}
+
+//Fungsi untuk About Screen
+function AboutScreen() {
+    const text = document.createElement("p");
+    text.textContent = "Welcome to About Page";
+    return text;
+}
