bd2d3c8b4285e8c216a7002ecaedeb3f7b24f0fc
rakabgs27 <rakabgs2000@gmail.com>
2023-02-13 17:04:42 +0700

    State

--- script.js
+++ script.js
@@ -3,16 +3,26 @@ function HomeScreen() {
     const navbar = Navbar();
 
     const textPreview = document.createElement("p");
+    textPreview.textContent = state.inputValue;
    
     const input = document.createElement("input");
+    input.id = "input";
+    input.value = state.inputValue;
     input.oninput = function (event) {
-        textPreview.textContent = event.target.value;
+        setState({ inputValue: event.target.value})
     };
     input.placeholder = "Enter your name";
-   
+    
+    const buttonClear = document.createElement("button");
+    buttonClear.textContent = "Clear";
+    buttonClear.onclick = function (){
+        setState({ inputValue: ""})
+    };
+
     const div = document.createElement("div");
     div.append(navbar);
     div.append(input);
+    div.append(buttonClear);
     div.append(textPreview);
 
     return div;
@@ -35,6 +45,16 @@ function AboutScreen() {
     return div;
 }
 
+let state = {
+    inputValue: "",
+    hash: location.hash,
+};
+
+function setState(newState) {
+    state = {...state,...newState};
+    render();
+}
+
 function Link(props) {
     const link = document.createElement("a");
     link.href = props.href;
@@ -42,7 +62,9 @@ function Link(props) {
     //Fungsi On Click untuk Link
     link.onclick = function(event) {
         event.preventDefault();
-        history.pushState(null,"",event.target.href)
+        const url = new URL(event.target.href)
+        setState({ hash: url.hash});
+        history.pushState(null,"",event.target.href);
         render();
     };
 
@@ -73,9 +95,9 @@ function App() {
     const aboutScreen = AboutScreen();
 
     //Fungsi Percabangan untuk melakukan pengisian manual pada URL
-    if (location.hash === "#about") {
+    if (state.hash === "#about") {
        return aboutScreen;
-    } else if (location.hash === "#home") {
+    } else if (state.hash === "#home") {
         return homeScreen;
     }
 }
@@ -83,8 +105,21 @@ function App() {
 function render() {
     const root = document.getElementById("root");
     const app = App();
+
+    const focusedElementId = document.activeElement.id;
+    const focusedElementSelectionStart = document.activeElement.selectionStart;
+    const focusedElementSelectionEnd = document.activeElement.selectionEnd;
+    
     root.innerHTML = "";
     root.append(app);
+
+    if(focusedElementId) {
+        const focusedElement = document.getElementById(focusedElementId);
+        focusedElement.focus();
+        focusedElement.selectionStart = focusedElementSelectionStart;
+        focusedElement.selectionEnd = focusedElementSelectionEnd;
+
+    }
 }
 
 render();
